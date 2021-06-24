4b7f516e50ec2ad4907e534372f2daf01a4051cf
Sam Morgen <samorgen14@gmail.com>
2021-06-23 20:13:54 -0700

    Added inquirer and first question

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-  console.log("Portfolio complete! Check out index.html to see the output");
-});
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output");
+// });
